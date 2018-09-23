cbb584d53223aeca29eda76f5a6afe3ab69e5935
AdamJHowell <adam.j.howell@gmail.com>
2018-09-23 00:08:58 -0600

    4 - This is literally working, but not sure if it meets the project requirements.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -10,6 +11,9 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class Controller
@@ -22,6 +26,7 @@ public class Controller
 	@FXML private Pane lowerLeft;
 	@FXML private Pane lowerRight;
 	@FXML private Button rollButton;
+	private List<DiceFace> rollList = new ArrayList<>();
 
 
 	@FXML void initialize()
@@ -36,12 +41,95 @@ public class Controller
 		lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
 		lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
 
+		for( int i = 1; i < 7; i++ )
+		{
+			rollList.add( new DiceFace( i ) );
+		}
+
 		rollButton.setOnAction( e ->
 			rollDice() );
 	}
 
+
 	@FXML private void rollDice()
 	{
-		middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+		Random rand = new java.util.Random();
+		rollList.get( rand.nextInt( 6 ) ).execute();
+	}
+
+
+	class DiceFace
+	{
+		private int number;
+
+
+		DiceFace( int num )
+		{
+			number = num;
+		}
+
+
+		private void execute()
+		{
+			switch( number )
+			{
+				case 1:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				case 2:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				case 3:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				case 4:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				case 5:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				case 6:
+					topCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					middleCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					bottomCenter.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					upperRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Blue" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerLeft.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					lowerRight.setBackground( new Background( new BackgroundFill( Color.valueOf( "Red" ), CornerRadii.EMPTY, Insets.EMPTY ) ) );
+					break;
+				default:
+					break;
+			}
+		}
 	}
 }
